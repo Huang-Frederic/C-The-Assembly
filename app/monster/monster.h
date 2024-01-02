87771c3e9b7ecd09e4529d9b6f68977abcb10263
arkayzdev <franck.zhuang@hotmail.com>
2024-01-02 20:22:19 +0100

    Monsters Files Process

---
+++ app/monster/monster.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Monster {
+    char name[30];
+    int hp;
+    int strength;
+    int dodge; // 1 = 25% dodge
+    int weak; // 1 = attack decreased by 25%
+    int defense; // Reduce damage received by x percent
+};
+
+void createMonster(struct Monster *m, char *name, int hp, int strength, int dodge, int weak, int defense);
+void getMonsterPath(char *name, char *path);
+int checkMonsterPath(char *path);
+int addMonster(struct Monster *m, char *path);
+void printMonster(struct Monster *m);
+int readMonster(char *name);
