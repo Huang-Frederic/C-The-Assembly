04baebc2278e45024f9e09022251bcf8fb7dd6fb
arkayzdev <franck.zhuang@hotmail.com>
2024-01-17 00:56:25 +0100

    DB CRUD

--- test/db.c
+++ test/db.c
@@ -1,6 +1,7 @@
 #include <sqlite3.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int callback(void *NotUsed, int argc, char **argv, char **azColName)
 {
@@ -9,6 +10,7 @@ int callback(void *NotUsed, int argc, char **argv, char **azColName)
 
     for (int i = 0; i < argc; i++)
     {
+        printf("%d\n", argc);
         printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
 
@@ -21,7 +23,151 @@ bool create()
 {
     sqlite3 *db;
     char *err_msg = 0;
-    char *sql = "CREATE TABLE IF NOT EXISTS score (id INTEGER PRIMARY KEY AUTOINCREMENT, score INTEGER, username CHAR(30))";
+    sqlite3_stmt *res;
+
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return false;
+    }
+
+    char *sql = "CREATE TABLE IF NOT EXISTS ranking (id INTEGER PRIMARY KEY AUTOINCREMENT, score INTEGER, username CHAR(30));";
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+
+    if (rc == SQLITE_OK)
+    {
+        sqlite3_step(res);
+        sqlite3_finalize(res);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+        return false;
+    }
+
+    sqlite3_close(db);
+    return true;
+}
+
+bool insert(int score, char *username)
+{
+    sqlite3 *db;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return false;
+    }
+
+    char *sql = "INSERT INTO ranking (score, username) VALUES (?, ?);";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+
+    if (rc == SQLITE_OK)
+    {
+        sqlite3_bind_int(res, 1, score);
+        sqlite3_bind_text(res, 2, username, sizeof(username) / sizeof(char), SQLITE_TRANSIENT);
+        sqlite3_step(res);
+        sqlite3_finalize(res);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+        return false;
+    }
+
+    sqlite3_close(db);
+    return true;
+}
+
+bool select_all()
+{
+
+    sqlite3 *db;
+    char *err_msg = 0;
+
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc != SQLITE_OK)
+    {
+
+        fprintf(stderr, "Cannot open database: %s\n",
+                sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+    char *sql = "SELECT * FROM ranking";
+
+    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
+
+    if (rc != SQLITE_OK)
+    {
+
+        fprintf(stderr, "Failed to select data\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+    sqlite3_close(db);
+
+    return 0;
+}
+
+bool update(int score, char *username)
+{
+    sqlite3 *db;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return false;
+    }
+
+    char *sql = "UPDATE ranking SET score=? WHERE username=?;";
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+
+    if (rc == SQLITE_OK)
+    {
+        sqlite3_bind_int(res, 1, score);
+        sqlite3_bind_text(res, 2, username, strlen(username) / sizeof(char), SQLITE_TRANSIENT);
+        sqlite3_step(res);
+        sqlite3_finalize(res);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+        return false;
+    }
+
+    sqlite3_close(db);
+    return true;
+}
+
+bool delete(char *username)
+{
+    sqlite3 *db;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+
     int rc = sqlite3_open("test.db", &db);
 
     if (rc != SQLITE_OK)
@@ -33,26 +179,67 @@ bool create()
         return false;
     }
 
-    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
+    char *sql = "DELETE FROM ranking WHERE username=?;";
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+    if (rc == SQLITE_OK)
+    {
+        sqlite3_bind_text(res, 1, username, strlen(username) / sizeof(char), SQLITE_TRANSIENT);
+        sqlite3_step(res);
+        sqlite3_finalize(res);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+        return false;
+    }
+
+    sqlite3_close(db);
+    return true;
+}
+
+bool delete_all()
+{
+    sqlite3 *db;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+
+    int rc = sqlite3_open("test.db", &db);
 
     if (rc != SQLITE_OK)
     {
-        fprintf(stderr, "Failed to select data\n");
-        fprintf(stderr, "SQL error: %s\n", err_msg);
 
-        sqlite3_free(err_msg);
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
 
         return false;
     }
 
-    sqlite3_close(db);
+    char *sql = "DELETE FROM ranking;";
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+    if (rc == SQLITE_OK)
+    {
+        sqlite3_step(res);
+        sqlite3_finalize(res);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+        return false;
+    }
 
+    sqlite3_close(db);
     return true;
 }
 
 int main(void)
 {
-    if (create())
-        return 0;
+    int score = 1024;
+    char *username = "ARRR";
+    // create();
+    // if (insert(score, username))
+    //     return 0;
+    select_all();
+
+    // delete_all();
+    return 0;
 }
